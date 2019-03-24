110226896b7ea808f2d8cc82a9b86a5beff65089
Samuel Darkhovsky <darkhovsky@usa.net>
2019-03-24 01:54:25 -0700

    debugging

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 # see https://www.linuxjournal.com/content/introduction-opengl-programming
 g++ -g -std=c++11 -I/home/sd/workspace/eigen-eigen-323c052e1731 PointCloudViewer.cpp point_cloud.cpp -lglut -lGL -lGLU -o PointCloudViewer
+./PointCloudViewer -i sample.xyz
